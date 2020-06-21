0b235df18209b4f94275a8e5fdad286bcb0d8b3f
kbu715 <kbu715@gmail.com>
2020-06-21 21:29:07 +0900

    ~2.1 useTitle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from 'react';
 
-function App() {
+// const useTitle = (initialTitle) => {
+//   const [title, setTitle] = useState(initialTitle);
+//   const titleUpdate = () => {
+//     const htmlTitle = document.querySelector("title");
+//     htmlTitle.innerText = title;
+//   }
+//   useEffect(titleUpdate, [title]);
+//   return setTitle;
+
+// }
+
+// const App = () => {
+//   const titleUpdater = useTitle('Loading...')
+//   setTimeout(()=>{titleUpdater("Home")}, 5000);
+//   return (
+//     <div>
+//       hi  
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { useState, useEffect, useRef } from 'react';
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e) => {
+    const { target : { value } } = e;
+    let willUpdate = true;
+    if(typeof validator === 'function'){
+      willUpdate = validator(value);
+    }
+    if(willUpdate){
+      setValue(value);
+    }
+  }
+
+  return { value, onChange };
+
+}
+
+const useTitle = initialTitle => {
+  const [title, setTitle] = useState(initialTitle);
+  const titleUpdate = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  }
+  useEffect(titleUpdate, [title]);
+
+  return setTitle;
+
+}
+
+const App = () => {
+  const titleUpdater = useTitle('Loading....')
+  setTimeout(()=>{titleUpdater('Home')}, 6000)
+
+  const maxLen = value=>value.length < 10;
+  const name = useInput('hi~ ', maxLen)
+
+  const potato = useRef();
+  setTimeout(()=>{potato.current.focus()},5000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hi</h1>
+      <input ref={potato} {...name} />
     </div>
   );
-}
+};
 
 export default App;
