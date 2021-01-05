160f42b2d3f0ada0da5f9c661c83c5042887fbbc
kbu715 <kbu715@gmail.com>
2021-01-05 22:42:43 +0900

    custom hooks remind

--- src/App.js
+++ src/App.js
@@ -1,110 +1,108 @@
-import React, { useState, useEffect } from 'react';
-
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const titleUpdate = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  }
-  useEffect(titleUpdate, [title]);
-  return setTitle;
-
-}
-
-const App = () => {
-  const titleUpdater = useTitle('Loading...')
-  setTimeout(()=>{titleUpdater("Home")}, 5000);
-  return (
-    <div>
-      hi
-    </div>
-  );
-};
-
-export default App;
-
-// import React, { useState, useEffect, useRef } from "react";
-
-// const useInput = (initialValue, validator) => {
-//   const [value, setValue] = useState(initialValue);
-
-//   const onChange = (e) => {
-//     const {
-//       target: { value },
-//     } = e;
-//     let willUpdate = true;
-//     if (typeof validator === "function") {
-//       willUpdate = validator(value);
-//     }
-//     if (willUpdate) {
-//       setValue(value);
-//     }
-//   };
-
-//   return { value, onChange };
-// };
+// import React, { useState, useEffect } from 'react';
 
 // const useTitle = (initialTitle) => {
 //   const [title, setTitle] = useState(initialTitle);
 //   const titleUpdate = () => {
 //     const htmlTitle = document.querySelector("title");
 //     htmlTitle.innerText = title;
-//   };
+//   }
 //   useEffect(titleUpdate, [title]);
-
 //   return setTitle;
-// };
 
-// const useClick = (onClick) => {
-//   const element = useRef();
-
-//   useEffect(() => {
-//     if (typeof onClick === "function") {
-//       if (element.current) {
-//         element.current.addEventListener("click", onClick);
-//       }
-//     }
-//     return () => {
-//       //componentWillUnmount 때 발생
-//       if (element.current) {
-//         element.current.removeEventListener("click", onClick);
-//       }
-//     };
-//   }, []);
-//   return element;
-// };
+// }
 
 // const App = () => {
-//   const titleUpdater = useTitle("Loading....");
-//   setTimeout(() => {
-//     titleUpdater("Home");
-//   }, 6000);
-
-//   const maxLen = (value) => value.length < 10;
-//   const name = useInput("hi~ ", maxLen);
-
-//   const potato = useRef();
-//   setTimeout(() => {
-//     potato.current.focus();
-//   }, 5000);
-
-//   const sayHello = () => console.log("Hello");
-//   const title = useClick(sayHello);
+//   const titleUpdater = useTitle('Loading...')
+//   setTimeout(()=>{titleUpdater("Home")}, 5000);
 //   return (
 //     <div>
-//       <h1 ref={title}>Hi</h1>
-//       <input ref={potato} {...name} />
+//       hi
 //     </div>
 //   );
 // };
 
 // export default App;
 
+import React, { useState, useEffect, useRef } from 'react';
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
 
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    let willUpdate = true;
+    if (typeof validator === 'function') {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
 
+  return { value, onChange };
+};
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const titleUpdate = () => {
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
+  useEffect(titleUpdate, [title]);
 
+  return setTitle;
+};
+
+const useClick = (onClick) => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (typeof onClick === 'function') {
+      if (element.current) {
+        element.current.addEventListener('click', onClick);
+      }
+    }
+
+    return () => {
+      //componentWillUnmount 때 발생
+      const clickEl = element;
+      if (clickEl) {
+        clickEl.current.removeEventListener('click', onClick);
+      }
+    };
+  }, [onClick]);
+  return element;
+};
+
+const App = () => {
+  const titleUpdater = useTitle('Loading....');
+  setTimeout(() => {
+    titleUpdater('Home');
+  }, 6000);
+
+  const maxLen = (value) => value.length < 10;
+  const name = useInput('hi~ ', maxLen);
+
+  const potato = useRef();
+  setTimeout(() => {
+    potato.current.focus();
+  }, 5000);
+
+  const changeText = (e) => {
+    e.target.innerText = 'Hello~';
+  };
+  const title = useClick(changeText);
+  return (
+    <div>
+      <h1 ref={title}>Hi</h1>
+      <input ref={potato} {...name} />
+    </div>
+  );
+};
+
+export default App;
 
 // import React, { useState } from 'react';
 
@@ -137,16 +135,12 @@ export default App;
 
 // export default App;
 
-
-
-
 // import React, { useState } from 'react';
-
 
 // const contents = [
 //     {
 //         tab : "Section 1",
-//         content : "I'm the content of Section 1" 
+//         content : "I'm the content of Section 1"
 //     },
 //     {
 //         tab : "Section 2",
@@ -171,9 +165,9 @@ export default App;
 //   return (
 //     <div>
 //       <h1>Hello</h1>
-//         {contents.map((section, index)=><button key={index} onClick={()=>{changeItem(index)}}>{section.tab}</button>)}    
+//         {contents.map((section, index)=><button key={index} onClick={()=>{changeItem(index)}}>{section.tab}</button>)}
 //         <br />
-//         {currentItem.content}  
+//         {currentItem.content}
 //     </div>
 //   );
 // };
